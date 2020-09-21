150612a575dea30d3a98970e4a97a58b1a6077ef
Abdel <juniorperezpy@gmail.com>
2020-09-21 15:02:47 -0400

    Add color to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropType from 'prop-types';
 
-const Button = ({ buttonName }) => (
-  <button type="button" className="btn">{buttonName}</button>
+const Button = ({ buttonName, color = 'orange' }) => (
+  <button type="button" className={`btn btn-${color}`} color={color}>{buttonName}</button>
 );
 
 Button.propTypes = { buttonName: PropType.string.isRequired };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,21 +2,35 @@ import React from 'react';
 import Button from './Button';
 
 const ButtonPanel = () => {
-  const buttonNames = {
-    0: ['AC', '+/', '%', '÷'],
-    1: ['7', '8', '9', 'X'],
-    2: ['4', '5', '6', '-'],
-    3: ['1', '2', '3', '+'],
-    4: ['0', '.', '='],
+  const btnProps = {
+    0: [{name: 'AC', color: 'lightgray'},
+        {name: '+/', color: 'lightgray'},
+        {name: '%', color: 'lightgray'},
+        {name: '÷'}],
+    1: [{name: '7', color: 'lightgray'},
+        {name: '8', color: 'lightgray'},
+        {name: '9', color: 'lightgray'},
+        {name: 'X'}],
+    2: [{name: '4', color: 'lightgray'},
+        {name: '5', color: 'lightgray'},
+        {name: '6', color: 'lightgray'},
+        {name: '-'}],
+    3: [{name: '1', color: 'lightgray'},
+        {name: '2', color: 'lightgray'},
+        {name: '3', color: 'lightgray'},
+        {name: '+'}],
+    4: [{name: '0', color: 'lightgray'},
+        {name: '.', color: 'lightgray'},
+        {name: '='}],
   };
 
   const renderedButtons = [];
   Object
-    .keys(buttonNames)
+    .keys(btnProps)
     .forEach(rowKey => {
       renderedButtons.push(
         <div key={rowKey} className="btn-group">
-          { buttonNames[rowKey].map(btnName => <Button key={btnName} buttonName={btnName} />) }
+          { btnProps[rowKey].map(btnProp => <Button key={btnProp.name} buttonName={btnProp.name} color={btnProp["color"]} />) }
         </div>,
       );
     });
