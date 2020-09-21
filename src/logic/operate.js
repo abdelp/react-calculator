6a8f63dc01e73a83db7fd5cb59acff8041ded560
Abdel <juniorperezpy@gmail.com>
2020-09-21 19:41:39 -0400

    Update operate to always expect two numbers as parameters

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,14 +3,14 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operation) => {
   try {
     numberOne = Big(numberOne);
-    if (numberTwo) numberTwo = Big(numberTwo);
+    numberTwo = Big(numberTwo);
 
     const operators = {
       '+': numberOne.plus.bind(numberOne),
       '-': numberOne.minus.bind(numberOne),
       'X': numberOne.times.bind(numberOne),
       '÷': numberOne.div.bind(numberOne),
-      '%': (numberTwo) => numberOne.times(numberTwo ? numberTwo : 1) / 100
+      '%': (numberTwo) => numberOne.times(numberTwo) / 100
     };
 
     const result = operators[operation](numberTwo);
