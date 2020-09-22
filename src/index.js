1842138f258c2d0fac63a37de84a4d7885e77fd3
Abdel <juniorperezpy@gmail.com>
2020-09-22 16:04:47 -0400

    Remove import of big.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './styles/scss/index.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import Big from 'big.js';
 
 ReactDOM.render(
   <React.StrictMode>
