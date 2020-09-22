eac9d7239b768a9425161ef432e5d82278e59ed5
Abdel <juniorperezpy@gmail.com>
2020-09-22 12:48:57 -0400

    Add options for digit and operator buttons

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,27 +1,53 @@
 import PropType from 'prop-types';
+import operate from './operate';
 
-const calculate = (data, bntName) => {
-  switch(bntName) {
-    case 'AC':
+const calculate = (data, buttonName) => {
+  let dataUpdated = {...data};
+  const prop = data.operator ? 'next' : 'total';
 
-      break;
-    case '-':
-
-      break;
-    case '/':
-
-      break;
-    case:
-
-      break;
-    case:
+  if (buttonName.match(/\d/)) {
+    dataUpdated[prop] = (Number(data[prop]) || '') + buttonName;
+  } else if (['+', '-', 'X', '÷'].includes(buttonName)) {
+    if(Number(data['next'])) {
+      dataUpdated = {
+        total: operate(data.total, data.next, data.operator),
+        next: null,
+        operator: buttonName,
+        buttonName: null
+      };
+    } else {
+      dataUpdated.operator = buttonName;
+    }
   }
+  // } else if (buttonName === '%') {
+  //   if (!next && next !== 0) next = 1;
+  //   total = operate(total, next, buttontName);
+  //   next = null;
+  //   operation = null;
+  // } else if (buttontName === '.') {
+  //   if (next) {
+  //     next = `${next}.`;
+  //   } else {
+  //     next = `${total}.`;
+  //   }
+  //   operation = null;
+  // } else if (buttontName === '=') {
+  //   if (total && next && operation) {
+  //     total = operate(total, next, operation);
+  //   }
+  //   operation = null;
+  //   next = null;
+  // } button == 'AC'
+
+
+  return dataUpdated;
 };
 
 calculate.propType = {
-  total: PropType.number.isRequired,
-  next: PropType.number.isRequired,
-  operation: PropType.string.isRequired
+  data: {total: PropType.number,
+  next: PropType.number,
+  operation: PropType.string},
+  buttonName: PropType.string.isRequired
 };
 
 export default calculate;
