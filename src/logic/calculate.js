c9aca71e4004e10acbbdb6e61c563b13bcbd5115
Abdel <juniorperezpy@gmail.com>
2020-09-22 16:05:39 -0400

    Fix error for digits, add functionality for the other buttons

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,51 +2,59 @@ import PropType from 'prop-types';
 import operate from './operate';
 
 const calculate = (data, buttonName) => {
+  if (data.total === 'ERROR' && buttonName !== 'AC') return;
   let dataUpdated = {...data};
   const prop = data.operator ? 'next' : 'total';
 
   if (buttonName.match(/\d/)) {
-    dataUpdated[prop] = (Number(data[prop]) || '') + buttonName;
+    dataUpdated[prop] = `${data[prop] || ''}`.replace(/^0[^\.]/, '') + buttonName;
   } else if (['+', '-', 'X', '÷'].includes(buttonName)) {
-    if(Number(data['next'])) {
+    if(data.next) {
       dataUpdated = {
         total: operate(data.total, data.next, data.operator),
         next: null,
-        operator: buttonName,
-        buttonName: null
+        operator: buttonName
       };
-    } else {
+    } else if (data.total) {
       dataUpdated.operator = buttonName;
     }
-  }
-  // } else if (buttonName === '%') {
-  //   if (!next && next !== 0) next = 1;
-  //   total = operate(total, next, buttontName);
-  //   next = null;
-  //   operation = null;
-  // } else if (buttontName === '.') {
-  //   if (next) {
-  //     next = `${next}.`;
-  //   } else {
-  //     next = `${total}.`;
-  //   }
-  //   operation = null;
-  // } else if (buttontName === '=') {
-  //   if (total && next && operation) {
-  //     total = operate(total, next, operation);
-  //   }
-  //   operation = null;
-  //   next = null;
-  // } button == 'AC'
+  } else if (buttonName === '%') {
+    if (!Number(data.next)) dataUpdated.next = 1;
 
+    dataUpdated = {
+      total: operate(dataUpdated.total, dataUpdated.next, buttonName),
+      next: null,
+      operator: buttonName
+    };
+  } else if (buttonName === '.') {
+    if (dataUpdated[prop].indexOf('.') === -1) {
+      dataUpdated[prop] = `${dataUpdated[prop]}.`;
+    }
+  } else if (buttonName === '=') {
+    if (dataUpdated['next']) {
+      dataUpdated = {
+        total: operate(dataUpdated.total, dataUpdated.next, dataUpdated.operator)
+      };
+    }
+    dataUpdated.operator = null;
+    dataUpdated.next = null;
+  } else if (buttonName === 'AC') {
+    dataUpdated = {
+      total: null,
+      next: null,
+      operator: null
+    };
+  }
 
   return dataUpdated;
 };
 
 calculate.propType = {
-  data: {total: PropType.number,
-  next: PropType.number,
-  operation: PropType.string},
+  data: {
+    total: PropType.number,
+    next: PropType.number,
+    operation: PropType.string
+  },
   buttonName: PropType.string.isRequired
 };
 
