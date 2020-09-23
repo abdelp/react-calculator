b25d323bbc0b348d092f028ba8556740ba71c48f
Abdel <juniorperezpy@gmail.com>
2020-09-23 14:55:11 -0400

    Add initial state to App component

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,13 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate'; // eslint-disable-line no-unused-vars
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {total: null,
+                  next: null,
+                  operation: null};
+  }
+
   render () {
     return <div className="App">
       <Display result="0" />
